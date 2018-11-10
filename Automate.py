d84c6313fe8af96a8daf46f9a33adce21ccca9c8
Apurva Nisal <apunisal@gmail.com>
2018-11-10 17:46:09 +0530

    Added Comments

--- Automate.py
+++ Automate.py
@@ -1,24 +1,37 @@
 import os
 
+#Take Username from user
 print("-----WELCOME-------")
+
+#Username and Password are same.
 username = raw_input("Enter the Username : ")
 print("Please note that username and password will be same.")
+
+#Enter into Vagrant Folder
 os.system("cd Ubuntu-18.04.1-Server")
+
+#Boot up All servers
 os.system("vagrant up")
+
+#Add User to all Servers 
 ansi = "ansible servers -a 'useradd "+username+" -shell /bin/bash -p "+username+"'"
 returned_value = os.system(ansi) 
+
+#Results of ansible
 print('returned value:', returned_value)
+
+#Choose you Servers where you want to work.
 print("Please Choose any one Server from 1 to 5")
 ch = raw_input("Server no : ")
 if ch=="1" : 
-    os.system("ssh "+ username +"@192.168.2.5")
+    os.system("ssh "+ username +"@server1.example.com")
 elif ch=="2":
-    os.system("ssh "+ username +"@192.168.2.6")
+    os.system("ssh "+ username +"@server2.example.com")
 elif ch=="3":
-    os.system("ssh "+ username +"@192.168.2.7")
+    os.system("ssh "+ username +"@server3.example.com")
 elif ch=="4":
-    os.system("ssh "+ username +"@192.168.2.8")
+    os.system("ssh "+ username +"@server4.example.com")
 elif ch=="5":
-    os.system("ssh "+ username +"@192.168.2.9")
+    os.system("ssh "+ username +"@server5.example.com")
 else:
     print("Enter Valid Number")
